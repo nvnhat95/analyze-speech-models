9b7dd4ce1e0379e41e204ae15203c20b35beb89d
nvnhat95 <nguyenvietnhat95@gmail.com>
2021-12-14 09:22:57 -0600

    tweat

--- classify.py
+++ classify.py
@@ -1,13 +1,15 @@
-import os, glob, argparse
+import os, glob, argparse, random
 import numpy as np
 import pandas as pd
 import h5py, pickle
 from sklearn import svm
 from sklearn import metrics
 import utils
+from scipy.spatial import distance
+from collections import defaultdict
 
 def svm_classification(embeddings_train, labels_train, embeddings_test, labels_test):
-#     #Create a svm Classifier
+    #Create a svm Classifier
     clf = svm.SVC(kernel='linear')
 
     #Train the model using the training sets
@@ -15,16 +17,43 @@ def svm_classification(embeddings_train, labels_train, embeddings_test, labels_t
 
     #Predict the response for test dataset
     y_pred = clf.predict(embeddings_test)
-
+    
     return metrics.f1_score(labels_test, y_pred, average='macro')
 
-#     label_set = list(set(labels_test))
 
-#     F1s = []
-#     for _ in range(100):
-#         y_pred = np.random.choice(label_set, len(labels_test))
-#         F1s.append(metrics.f1_score(labels_test, y_pred, average='macro'))
-#     return np.mean(F1s)
+def svm_regression(embeddings_train, labels_train, embeddings_test, labels_test):
+    #Create a svm Classifier
+    clf = svm.SVR(kernel='linear')
+
+    #Train the model using the training sets
+    clf.fit(embeddings_train, labels_train)
+
+    #Predict the response for test dataset
+    score = clf.score(embeddings_test, labels_test)
+    
+    return score
+
+
+def ABX_analysis(embeddings, labels):
+    score = 0
+    pos_dict = defaultdict(set)
+    
+    all_pos = set(range(len(labels)))
+    
+    for i in range(len(labels)):
+        pos_dict[labels[i]].add(i)
+    
+    for i in range(len(labels)):
+        for _ in range(10):
+            while True:
+                j = np.random.choice(list(pos_dict[labels[i]]))
+                if j != i:
+                    break
+            k = np.random.choice(list(all_pos - pos_dict[labels[i]]))
+            d_ij = distance.cosine(embeddings[i], embeddings[j])
+            d_ik = distance.cosine(embeddings[i], embeddings[k])
+            score += d_ij < d_ik
+    return score / len(labels) / 10
 
     
 def vis_tSNE(embeddings, labels):
@@ -75,15 +104,23 @@ def load_data_acoustic(args):
     for i, row in df_train.iterrows():
         key = row['wav_id'] + '-' + args.feature_name
         if key in h5_train:
+            if np.any(np.isnan(h5_train[key][:])):
+                continue
             embeddings_train.append(np.mean(h5_train[key][:], axis=0))
         else:
+            if np.any(np.isnan(h5_test[key][:])):
+                continue
             embeddings_train.append(np.mean(h5_test[key][:], axis=0))
     
     for i, row in df_test.iterrows():
         key = row['wav_id'] + '-' + args.feature_name
         if key in h5_train:
+            if np.any(np.isnan(h5_train[key][:])):
+                continue
             embeddings_test.append(np.mean(h5_train[key][:], axis=0))
         else:
+            if np.any(np.isnan(h5_test[key][:])):
+                continue
             embeddings_test.append(np.mean(h5_test[key][:], axis=0))
     
     h5_train.close()
@@ -100,13 +137,20 @@ def load_data_linguistic(args):
             with open("data/TIMIT/valid_words.pkl", 'rb') as f:
                 valid_words = pickle.load(f)
 
-        for key in h5:
+        keys = list(h5.keys())
+        ## Only use 20% because of too many
+        if args.label_column == 'phoneme':
+            keys = random.sample(keys, int(len(keys) * 0.2))
+                
+        for key in keys:
             if key.split('-')[-1] != args.feature_name:
                 continue
             if args.label_column == 'word':
                 word = key.split('-')[-2].split('_')[0]
                 if word not in valid_words:
                     continue
+            if np.any(np.isnan(h5[key][:])):
+                continue
             embeddings.append(h5[key][:])
             labels.append(key.split('-')[-2].split('_')[0])
         h5.close()
@@ -118,10 +162,32 @@ def load_data_linguistic(args):
     return embeddings_train, labels_train, embeddings_test, labels_test
     
     
+def select_neurons(args, embeddings):
+    if args.top_neurons != 0:
+        if args.label_column in ['speaker', 'dialect', 'gender']:
+            feat = 'acoustic'
+        else:
+            feat = 'linguistic'
+            
+        feat = 'acoustic'
+        n_top = abs(args.top_neurons) #/ 100 * len(ranking[args.feature_name][feat])
+#         print(n_top)
+        if args.top_neurons > 0:
+            selected_neurons =  ranking[args.feature_name][feat] <= n_top
+        else:
+            selected_neurons = ranking[args.feature_name][feat] >= len(ranking[args.feature_name][feat]) - n_top
+        for i in range(len(embeddings)):
+            embeddings[i] = embeddings[i][selected_neurons]
+            
+    return embeddings
+    
+    
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Train classifer on top of pretrained features')
     parser.add_argument('--seed', type=int, default=42,
                         help='random seed')
+    parser.add_argument('--top_neurons', type=int, default=0,
+                        help='number of neurons on top of ranking (negative ~ bottom), used for classification')
     parser.add_argument('--df_train', type=str,
                         help='metadata file containing audio id and their properties')
     parser.add_argument('--df_test', type=str,
@@ -134,18 +200,37 @@ if __name__=='__main__':
                         help='h5 file containing extracted features')
     parser.add_argument('--h5_test', type=str,
                         help='h5 file containing extracted features')
-    parser.add_argument('--model', type=str, default='svm', choices=['svm'],
+    parser.add_argument('--model', type=str, default='svm', choices=['svm', 'abx', 'svr'],
                         help='probing model')
     
     args = parser.parse_args()
 
     utils.set_random_seed(args.seed)
     
-    if args.label_column in ['speaker', 'dialect', 'gender']:
-        embeddings_train, labels_train, embeddings_test, labels_test = load_data_acoustic(args)
+    with open("neuron_ranking/fix_speaker-sentence/wav2vec2_small.pkl", 'rb') as f:
+        ranking = pickle.load(f)
+    
+    if args.label_column in ['speaker', 'dialect', 'gender', 'length']:
+        embeddings_train, labels_train, embeddings_test, labels_test = load_data_acoustic(args)        
     elif args.label_column in ['phoneme', 'word']:
         embeddings_train, labels_train, embeddings_test, labels_test = load_data_linguistic(args)
+        
+#     embeddings_train = select_neurons(args, embeddings_train)
+#     embeddings_test = select_neurons(args, embeddings_test)
+    
+#     print("Train size:", len(embeddings_train))
+#     print("Test size:", len(embeddings_test))
+
+    print("Num classes: ", len(set(labels_train)))
     
     if args.model == 'svm':
         acc = svm_classification(embeddings_train, labels_train, embeddings_test, labels_test)
-        print(f"{args.feature_name} predict {args.label_column} with F1: {round(acc, 4)}")
+        print(f"{args.feature_name} predicts {args.label_column} with F1: {round(acc, 4)}")
+        
+    if args.model == 'svr':
+        acc = svm_regression(embeddings_train, labels_train, embeddings_test, labels_test)
+        print(f"{args.feature_name} regress {args.label_column} with score: {round(acc, 4)}")
+        
+    if args.model == 'abx':
+        acc = ABX_analysis(embeddings_test, labels_test)
+        print(f"{args.feature_name} analyzes {args.label_column} with ABX score: {round(acc, 4)}")
